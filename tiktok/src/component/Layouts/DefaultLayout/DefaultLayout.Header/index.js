8c19ebff938c0e027ba242ac1f328aef45db24a6
Phongnguyen2609 <Phongntth2102008@fpt.edu.vn>
2023-02-01 21:31:18 +0700

    Bài 6: Xây dựng logo + search phần header

--- tiktok/src/component/Layouts/DefaultLayout/DefaultLayout.Header/index.js
+++ tiktok/src/component/Layouts/DefaultLayout/DefaultLayout.Header/index.js
@@ -1,5 +1,41 @@
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import images from '~/assets/images';
+import styles from './Header.module.scss';
+
+const cx = classNames.bind(styles);
 function Header() {
-    return <h2>Header</h2>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                {/* Images Logo*/}
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="tiktok" />
+                </div>
+
+                {/* Search*/}
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and videos" spellCheck={false} />
+                    {/* icon close */}
+                    <button className={cx('close')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    {/* icon loading */}
+                    <button type="" className={cx('loading')}>
+                        <FontAwesomeIcon icon={faSpinner} />
+                    </button>
+                    {/* search-btn */}
+                    <button type="" className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+
+                {/* Actions */}
+                <div className={cx('actions')}></div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
